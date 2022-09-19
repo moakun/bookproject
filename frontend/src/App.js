302a87ef3030321d8b441586d48502b8fc7f3baf
moatassim <moatassimbarakat@gmail.com>
2022-09-19 23:41:47 +0100

    creating

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">shopbooks</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;
