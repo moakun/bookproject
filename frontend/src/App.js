c3a126c5c69a88fd9e096dff01df54670bd20f49
moatassim <moatassimbarakat@gmail.com>
2022-09-21 21:10:50 +0100

    advancement1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">shopbooks</a>
-      </header>
-      <main>list Products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Booky</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,26 @@
+const data = {
+  products: [
+    {
+      name: 'book',
+      slug: 'book',
+      category: 'old books',
+      image: '/images/img1.png',
+      price: '20',
+      countInStock: '10',
+      rating: '5',
+      numReviews: 10,
+    },
+    {
+      name: 'book',
+      slug: 'book2',
+      category: 'old books',
+      image: '/images/img1.png',
+      price: '20',
+      countInStock: '10',
+      rating: '5',
+      numReviews: 10,
+    },
+  ],
+};
+
+export default data;
